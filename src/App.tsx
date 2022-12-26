0be3a0f6d323c2f70ae2be4269bdbf4d54a11a8c
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-12-26 16:14:45 -0500

    Add csv parsing
    [Parsing library](https://github.com/Bunlong/react-papaparse) for
    uploading csv files to browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { CSVReader} from "./csv_parser/csv_parser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CSVReader></CSVReader>
+    </>
   );
 }
 
